b23357030f719124d0161dee73d1d4399d233d3c
ZeIcaro <89083008+ZeIcaro@users.noreply.github.com>
2024-11-17 21:29:26 -0300

    Automatize grid creation and get user input
    Created script to prompt the user to input grid
    size. Adjusted the grid creation function to
    automatize grid columns on css. It does this by
    changing a root variable that was created in the
    css file.
    The script that changes block styles on mouseover
    was also changed and put into its own function,
    and now generates random colors for each block.

--- eas-game.js
+++ eas-game.js
@@ -7,30 +7,66 @@ function createMultipleDivs (n) {
         const block = document.createElement("div");
         block.setAttribute("class", "block".toString());
         block.setAttribute("id", "block" + (i + 1).toString());
-        block.textContent = "test" + (i + 1).toString();
+        // block.textContent = "test" + (i + 1).toString();
         container.appendChild(block);
+    }
+}
+
+
+// Grid Configurator
+
+function configureGrid (ncol) {
+
+    // Clean up previous grid
+    while (container.firstChild) {
+        container.firstChild.remove();
     };
-};
 
-createMultipleDivs (16)
+    // Create new grid
+    ncolAdjusted = Math.min(100, ncol);
+    createMultipleDivs (ncolAdjusted ** 2);
+    
+    // Configure grid columns
+    document.documentElement.style.setProperty("--columns-row", ncolAdjusted);
 
-// Change colors of divs when mouseover
+    // Run blockStyles function to configure the blocks
+    blockStyles();
 
-let blocksArray = document.querySelectorAll(".block");
+}
 
-blocksArray.forEach(function(elem) {
-    elem.addEventListener("mouseover", () => {
-        elem.setAttribute("style", "background: black");
+
+// Change blocks styles on mouseover
+
+function blockStyles () {
+    let blocksArray = document.querySelectorAll(".block");
+
+    blocksArray.forEach(function(elem) {
+        var red = Math.random()*255;
+        var green = Math.random()*255;
+        var blue = Math.random()*255;
+
+        elem.addEventListener("mouseover", () => {
+            elem.setAttribute("style", `background-color: rgb(${red}, ${green}, ${blue})`);
+        });
+        
+        /* elem.addEventListener("mouseout", () => {
+            elem.setAttribute("style", "background: azure");
+        }); */
     });
-    elem.addEventListener("mouseout", () => {
-        elem.setAttribute("style", "background: azure");
-    });
-});
+}
+
+
 
 // Configure button to receive grid size paramenters
 
 const gridSizeButton = document.querySelector("#gridSizeButton");
 
-
+gridSizeButton.addEventListener("click", () => {
+    var ncol = prompt("Set grid size:");
+    if (ncol === null) {
+        return;
+    } else {
+        configureGrid(ncol);
+    }});
 
 
